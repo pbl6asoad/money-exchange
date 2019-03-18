26a022323ff73c1a861de11e7a4acde5e0fadd3a
pbl6asoad <44265632+pbl6asoad@users.noreply.github.com>
2019-03-19 01:49:07 +0300

    feat: add money exchanger

--- src/index.js
+++ src/index.js
@@ -1,5 +1,55 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    var err0r = {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    var exchange = {};
+    var emptyObj = {};
+    if(currency>10000){
+        return err0r;
+    }
+    else if(currency<0){
+        return emptyObj;
+    }
+    else{
+        exchange.H = Math.floor(currency/50);
+        currency = currency - exchange.H*50;
+        exchange.Q = Math.floor(currency/25);
+        currency = currency - exchange.Q*25;
+        exchange.D = Math.floor(currency/10);
+        currency = currency - exchange.D*10;
+        exchange.N = Math.floor(currency/5);
+        currency = currency - exchange.N*5;
+        exchange.P = Math.floor(currency/1);
+        currency = currency - exchange.P*1;
+        if(exchange.H == 0){
+            delete exchange.H;
+        }
+        else{
+        
+        }
+        if(exchange.Q == 0){
+            delete exchange.Q;
+        }
+        else{
+        
+        }
+        if(exchange.D == 0){
+            delete exchange.D;
+        }
+        else{
+        
+        }
+        if(exchange.N == 0){
+            delete exchange.N;
+        }
+        else{
+        
+        }
+        if(exchange.P == 0){
+            delete exchange.P;
+        }
+        else{
+        
+        }
+    }
+    return exchange;
 }
